02c623ad06c88dabf2c281ee0f867da9452832ed
Nhatnv0378 <21520378@ms.uit.edu.vn>
2022-08-05 21:06:42 +0700

    add delete account php

---
+++ admin/delete.php
@@ -0,0 +1,68 @@
+<?php
+    if (isset($_POST["id"]) && !empty($_POST["id"]))
+    {
+        require_once "../config/config.php";
+        
+        $query = "DELETE FROM Account WHERE user_id = ?";
+
+        if ($stmt = $mysqli->prepare($query))
+        {
+            $stmt->bind_param("i", $param_id);
+            $param_id = trim($_POST["id"]);
+
+            if ($stmt->execute())
+            {
+                header("location: admin.php");
+                exit();
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }            
+        }
+
+        $stmt->close();
+        $mysqli->close();
+    } else {
+        if (empty(trim($_GET["id"])))
+        {
+            header("location: error.php");
+            exit();
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Delete record</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <style>
+            .wrapper {
+                width: 600px;
+                margin: 0 auto;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="mt-5 mb-3"> Delete Record </h2>
+                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                            <div class="alert alert-danger">
+                                <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
+                                <p>Are you sure you want to delete this record?</p>
+                                <p>
+                                    <input type="submit" value="yes" class="btn btn-danger">
+                                    <a href="admin.php" class="btn btn-secondary m1-2">No</a>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>

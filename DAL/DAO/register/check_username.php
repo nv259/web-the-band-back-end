61140eb402bba5fed2d3365d49a1f3b76488f552
Nhatnv0378 <21520378@ms.uit.edu.vn>
2022-08-05 16:46:27 +0700

    seperate into 2 layer

---
+++ DAL/DAO/register/check_username.php
@@ -0,0 +1,30 @@
+<?php
+    $query = "SELECT user_id FROM Account WHERE username = ?";
+
+    if ($stmt = $mysqli->prepare($query))
+    {
+        $stmt->bind_param("s", $param_username);
+        
+        // Set parameter
+        $param_username = $instance->validation($_POST["username"]);
+
+        if ($stmt->execute())
+        {
+            // store result
+            $stmt->store_result();
+
+            if ($stmt->num_rows() == 1)
+            {
+                // this username is already taken
+                $username_error = "This username is already taken.";
+                echo "<script type='text/javascript'>alert('$username_error');</script>";
+            }
+            else {
+                $username = trim($_POST["username"]);
+            }
+        }
+        else echo "Oops! Something went wrong. Please try again later.";
+
+        $stmt->close();
+    }
+?>
--- register.php
+++ register.php
@@ -16,34 +16,7 @@
         // echo "Still worked";
         // echo htmlspecialchars($_SERVER["PHP_SELF"]);
 
-        $query = "SELECT user_id FROM Account WHERE username = ?";
-
-        if ($stmt = $mysqli->prepare($query))
-        {
-            $stmt->bind_param("s", $param_username);
-            
-            // Set parameter
-            $param_username = $instance->validation($_POST["username"]);
-
-            if ($stmt->execute())
-            {
-                // store result
-                $stmt->store_result();
-
-                if ($stmt->num_rows() == 1)
-                {
-                    // this username is already taken
-                    $username_error = "This username is already taken.";
-                    echo "<script type='text/javascript'>alert('$username_error');</script>";
-                }
-                else {
-                    $username = trim($_POST["username"]);
-                }
-            }
-            else echo "Oops! Something went wrong. Please try again later.";
-
-            $stmt->close();
-        }
+        include_once "./DAL/DAO/register/check_username.php";
 
         // Validate password
         if (strlen(trim($_POST["password"])) < 6)
